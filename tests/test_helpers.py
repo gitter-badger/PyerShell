161a5790bb80eb68816a00a9900674adabe3e158
Zhang Cheng <13501393281@163.com>
2017-02-19 14:17:49 +0000

    add test and fix the none type

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -15,8 +15,15 @@ class TestToBashStr:
         """
         assert gf.to_bash_str(python_object=1) == '1'
 
+    def test_none(self):
+        """
+        test for none object
+        """
+        assert gf.to_bash_str(python_object=None) == ""
+
+
     def test_list(self):
         """
         test for complex list structure
         """
-        assert gf.to_bash_str([1, "test", [1, 2]]) == '1 "test" 1 2'
+        assert gf.to_bash_str([1, "test", [1, None]]) == '1 "test" 1 '
