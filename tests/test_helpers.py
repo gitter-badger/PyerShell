fccc8587f058fc5ac9c8e6b580fa2ac46694547e
Zhang Cheng <13501393281@163.com>
2017-02-19 20:05:24 +0000

    add test file

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -2,7 +2,6 @@ import scr.helpers.general_function as gf
 
 
 class TestToBashStr:
-
     def test_str(self):
         """
         test for string object
@@ -21,9 +20,28 @@ class TestToBashStr:
         """
         assert gf.to_bash_str(python_object=None) == ""
 
-
     def test_list(self):
         """
         test for complex list structure
         """
         assert gf.to_bash_str([1, "test", [1, None]]) == '1 "test" 1 '
+
+
+class TestNotStrContext:
+    def test_simple_string(self):
+        assert not gf.not_in_string_context(substring="te", statement_string='"test" this')
+
+    def test_simple_non_string(self):
+        assert gf.not_in_string_context(substring="te", statement_string='test "this"')
+
+    def test_between_two_string(self):
+        assert gf.not_in_string_context(substring="te", statement_string='"that" test "this"')
+
+    def test_escape(self):
+        assert gf.not_in_string_context(substring="te", statement_string='"that" \\"test\\" "this"')
+
+    def test_single_quotes(self):
+        assert not gf.not_in_string_context(substring="te", statement_string="'test' this")
+
+    def test_mixed_double_single(self):
+        assert gf.not_in_string_context(substring="ha", statement_string="'this' \' \" ha \" \' \"that\" ")
