a9c1a19a7f9f898328b8bf8db1e31db37e8b3c4e
Zhang Cheng <13501393281@163.com>
2017-02-19 11:38:07 +0000

    add a function to convert python object to bash string

---
+++ scr/helpers/general_function.py
@@ -0,0 +1,22 @@
+
+def to_bash_str(python_object: object) -> str:
+    """
+    convert a python object to bash
+    this is used when you want to embed other statement in shell statement
+    :param python_object: the input python object
+    :return: the corresponding string
+    """
+
+    if isinstance(python_object, list):
+        # if it is a list, join them via space
+        return " ".join(to_bash_str(python_object))
+
+    elif isinstance(python_object, str):
+        # if the object is an string
+        # wrap it with quotes
+        return '"{0}"'.format(python_object)
+
+
+    else:
+        # else simply return the str
+        return str(python_object)
