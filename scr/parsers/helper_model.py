767d37a8afd6fb0e9995ec8ffa6858dcecb20168
Zhang Cheng <13501393281@163.com>
2017-02-19 14:07:01 +0000

    add three helper model for parsing

---
+++ scr/parsers/helper_model.py
@@ -0,0 +1,42 @@
+from enum import Enum
+
+
+class ParseMethod(Enum):
+    """"
+    this is the class indicates all the parse method we try to use to parse a statement
+    """
+    empty_statement = 0
+    python_statement = 1
+    python_in_shell_syntax = 2
+    shell_statement = 3
+
+
+class ParseFailure:
+    def __init__(self, parse_method: ParseMethod, failure_type: str, failure_message: str):
+        """
+        this is the class indicates one parse failure
+        since we try to parse a statement in four ways,
+        therefore four ParseFailure object will be in statement.failure
+
+        :param parse_method: the method we currently using
+        :param failure_type: the error type of the error encountered when parsing
+        :param failure_message: the error message of the error encountered when parsing
+        """
+        self.parse_method = parse_method
+        self.failure_type = failure_type
+        self.failure_message = failure_message
+
+
+# this is the named tuple that keeps track of all the embedded statement in a statement
+# notice, pipline is also handled by
+class EmbeddedStructure:
+    def __init__(self, embedded_str: str, embedded_value: object):
+        """
+        this is the named tuple that keeps track of all the embedded statement in a statement
+        notice, pipline is also handled by embedded structure
+
+        :param embedded_str: the string of the embedded structure
+        :param embedded_value: the evaluated value of the embedded structure
+        """
+        self.embedded_str = embedded_str
+        self.embedded_value = embedded_value
