293184ed5daa84383071ba82deec99bcf4fed01f
Zhang Cheng <13501393281@163.com>
2017-02-20 16:09:10 +0000

    update right margin to follow PEP8

--- tests/test_statement.py
+++ tests/test_statement.py
@@ -3,29 +3,39 @@ from scr.parsers.statement import Statement
 
 class TestHandlePipline:
     @staticmethod
-    def __handle_pipline_easy__(statement_str: str, pipline_value: object) -> str:
+    def __easy_pip__(statement_str: str, pipline_value: object) -> str:
         """
-        this is a helper to construct a statement and then run handle_pipline, then get the result
+        this is a helper to construct a statement and then run handle_pipline,
+        then get the result
         :param statement_str: the statement string
         :param pipline_value: the value that is piped into the statements
         :return: the evaluated statement string after pipline is handled
         """
-        test_statment = Statement(statement_str=statement_str, pipline_value=pipline_value)
-        test_statment.__handle_pipline__()
-        return test_statment.__evaled_statement_str__
+        test_statement = Statement(
+            statement_str=statement_str,
+            pipline_value=pipline_value)
+        test_statement.__handle_pipline__()
+        return test_statement.__evaled_statement_str__
 
     def test_pipline_marker(self):
         """
         test when people uses PIPLINE_VALUE_PLACEHOLDER
         """
-        result = self.__handle_pipline_easy__(statement_str="[$_ + i for i in range(2)]", pipline_value=1)
+        result = self.__easy_pip__(
+            statement_str="[$_ + i for i in range(2)]",
+            pipline_value=1)
+
         assert result == "[$(self.__pipline_value__) + i for i in range(2)]"
 
     def test_positional_para(self):
         """
-        test to handle pipline by add the pipline_value to the first positional parameter
+        test to handle pipline by add the pipline_value to the
+        first positional parameter
         """
-        result = self.__handle_pipline_easy__(statement_str="ls", pipline_value="test/")
+        result = self.__easy_pip__(
+            statement_str="ls",
+            pipline_value="test/")
+
         assert result == "ls $(self.__pipline_value__)"
 
     def test_keyword_para(self):
